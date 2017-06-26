e0975c813a761115b67a414a173a1ba623371436
grotser <dima@cg.solutions>
2017-06-26 15:55:20 +0300

    Create Calculate

---
+++ src/main/java/Calculate1.java
@@ -0,0 +1,15 @@
+/**
+ * Created by grots on 26-Jun-17.
+ */
+public class Calculate1 {
+    public static void main (String[] arg)
+    {
+        System.out.println("Calculate...");
+        int first = Integer.valueOf(arg[0]);
+        int second = Integer.valueOf(arg[1]);
+        int summ = first + second;
+        System.out.println("summ  " + summ );
+
+    }
+
+}
